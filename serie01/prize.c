3be3371d434c719173188bd84cab6a9b86944333
sebastian <sebastian.matkovich@gmail.com>
2019-03-01 19:57:21 +0100

    1.2

---
+++ serie01/prize.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+  {
+    double x;
+    int n;
+    printf("enter amount of prize in euro\n");
+    scanf("%lf", &x);
+    printf("enter number of friends\n");
+    scanf("%d", &n);
+    printf("everybody %lf \n", (double) x/(n+1));
+    return 0;
+  }
+
