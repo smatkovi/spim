e325496c806132dc726b42d5b3ec042275ea4340
sebastian <sebastian.matkovich@gmail.com>
2019-03-02 09:39:58 +0100

    1.7

--- serie01/sort3.c
+++ serie01/sort3.c
@@ -1,21 +1,53 @@
 #include<stdio.h>
 
+double a,b,c;
+void swap(double *, double *);
 int main()
   {
-    double a,b;
-    printf("enter length a of rectangle\n");
+
+
+    printf("enter number a\n");
     if(scanf("%lf", &a)!=1)
       {
         printf("you didn't enter a number\n");
         return 1;
       }
-    printf("enter width b of rectangle\n");
+    printf("enter number b\n");
     if(scanf("%lf", &b)!=1)
       {
         printf("you didn't enter a number\n");
         return 1;
       }
-    printf("The area of the rectangle is %lf units\n", a*b);
+    printf("enter number c\n");
+    if(scanf("%lf", &c)!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+
+
+    if(b>a)
+      {
+        swap(&b, &a);
+        printf("a and b swapped\n");
+      }
+    if(c>a)
+      {
+        swap(&c, &a);
+        printf("a and c swapped\n");
+      }
+    if(c>b)
+      {
+        swap(&b, &c);
+        printf("c and b swapped\n");
+      }
+    printf("%lf, %lf, %lf\n", a, b, c);
     return 0;
   }
 
+void swap(double *x, double *y)
+  {
+    double temp=*x;
+    *x=*y;
+    *y=temp;
+  }
