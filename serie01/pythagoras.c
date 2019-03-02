6ada469815754f9e316990ffaf4a0d18ce7ac407
sebastian <sebastian.matkovich@gmail.com>
2019-03-02 12:21:27 +0100

    1.8

--- serie01/pythagoras.c
+++ serie01/pythagoras.c
@@ -4,32 +4,45 @@
 int main()
   {
     int a,b,c;
-    long double temp;
+    double temp;
     printf("enter a natural number a\n");
     scanf("%lf", &temp);
     a = (int) temp;
     if(a==temp)
-    {
-      printf("enter a natural number b\n");
-      scanf("%lf", &temp);
-      b = (int) temp;
-      if(b==temp)
-        {
-          printf("enter a natural number c\n");
-          scanf("%lf", &temp);
-          c = (int) temp;
-            if(c==temp)
-              {
-                if(pow(a,2)+pow(b,2)==pow(c,2))
-                  printf("you entered a pythagorean triple \n");
-                else
-                  printf("you didn't enter a pythagorean triple\n");
-              }
-            else printf("you didn't enter an integer\n");
-        }
-      else printf("you didn't enter an integer\n");
-    }
-    else printf("you didn't enter an integer\n");
+      {
+        printf("enter a natural number b\n");
+        scanf("%lf", &temp);
+        b = (int) temp;
+        if(b==temp)
+          {
+            printf("enter a natural number c\n");
+            scanf("%lf", &temp);
+            c = (int) temp;
+              if(c==temp)
+                {
+                  if(pow(a,2)+pow(b,2)==pow(c,2))
+                    printf("you entered a pythagorean triple \n");
+                  else
+                    printf("you didn't enter a pythagorean triple\n");
+                }
+              else
+                {
+	          printf("you didn't enter an integer\n");
+	          return 1;
+	        }
+          }
+        else
+          {
+            printf("you didn't enter an integer\n");
+	    return 1;
+          }
+      }
+    else
+      {
+        printf("you didn't enter an integer\n");
+        return 1;
+      }
+
     return 0;
   }
 
--- serie01/triangle.c
+++ serie01/triangle.c
@@ -1,35 +1,76 @@
 #include<stdio.h>
 #include<math.h>
 
+double a,b,c;
+void swap(double *, double *);
+void normalchecks(double *, double *, double *);
+
 int main()
   {
-    int a,b,c;
-    long double temp;
-    printf("enter a natural number a\n");
+    double temp;
+    printf("enter edge length a\n");
     scanf("%lf", &temp);
-    a = (int) temp;
+    a = (double) temp;
     if(a==temp)
-    {
-      printf("enter a natural number b\n");
-      scanf("%lf", &temp);
-      b = (int) temp;
-      if(b==temp)
-        {
-          printf("enter a natural number c\n");
-          scanf("%lf", &temp);
-          c = (int) temp;
-            if(c==temp)
-              {
-                if(pow(a,2)+pow(b,2)==pow(c,2))
-                  printf("you entered a pythagorean triple \n");
-                else
-                  printf("you didn't enter a pythagorean triple\n");
-              }
-            else printf("you didn't enter an integer\n");
-        }
-      else printf("you didn't enter an integer\n");
-    }
-    else printf("you didn't enter an integer\n");
+      {
+        printf("enter edge length b\n");
+        scanf("%lf", &temp);
+        b = (double) temp;
+        if(b==temp)
+          {
+            printf("enter edge length c\n");
+            scanf("%lf", &temp);
+            c = (double) temp;
+              if(c==temp)
+                {
+		  if(b>a) swap(&b, &a);
+		  if(c>a) swap(&c, &a);
+		  if(c>b) swap(&b, &c);
+
+		  if(b+c==a) printf("triangle is one-dimensional degenerate\n");
+		  else normalchecks(&a, &b, &c);
+		  
+		  if(b+c<a) printf("triangle has impossible dimensions\n");
+		  else normalchecks(&a, &b, &c);
+                }
+              else
+                {
+	          printf("you didn't enter an integer\n");
+	          return 1;
+	        }
+          }
+        else
+          {
+            printf("you didn't enter an integer\n");
+	    return 1;
+          }
+      }
+    else
+      {
+        printf("you didn't enter an integer\n");
+        return 1;
+      }
+
     return 0;
   }
 
+void swap(double *x, double *y)
+  {
+    double temp=*x;
+    *x=*y;
+    *y=temp;
+  }
+
+void normalchecks(double *x, double *y, double *z)
+  {
+    if(a!=b && b!=c && c!=a) printf("triangle is scalene\n"); 
+      else 
+        {
+	  if(pow(a,2)+pow(b,2)==pow(c,2)) printf("triangle is right-angled\n");
+          else
+	    {	  
+	      if(a==b || b==c) printf("triangle is isosceles\n");
+              else if(a==b && b==c) printf("triangle is equilateral\n");
+            }			
+        }
+  }	    
