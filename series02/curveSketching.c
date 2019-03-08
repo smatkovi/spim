b373074bc2622b53b4db75e7ad0d3ffbd712ed40
sebastian <sebastian.matkovich@gmail.com>
2019-03-08 18:29:02 +0100

    2.2

---
+++ series02/curveSketching.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+  {
+    double a,b,c;
+    printf("enter a from a*x^2 term\n");
+    scanf("%lf", &a);
+        printf("enter  b from b*x^1 term\n");
+        scanf("%lf", &b);
+            printf("enter c from c*x^0 term\n");
+            scanf("%lf", &c);
+
+                    printf("function crosses y axis at (0, %lf) \n", c);
+                    if(pow(b, 2)>=4.*a*c) printf("function crosses x axis at (%lf, 0) and  (%lf, 0) \n", (-b+sqrt(pow(b, 2)-4.*a*c)/(2.*a)), (-b-sqrt(pow(b, 2)-4.*a*c)/(2.*a));
+    return 0;
+  }
+
