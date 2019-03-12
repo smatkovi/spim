a0501ccf12467af84508b8dc220d9b3d00357fb1
sebastian <sebastian.matkovich@gmail.com>
2019-03-12 21:52:24 +0100

    2.4

--- series02/quadrant.c
+++ series02/quadrant.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 double a,b;
+void normaltest(double x, double y);
 int main()
   {
     printf("enter a\n");
@@ -16,8 +17,14 @@ int main()
         return 1;
       }
 
-    if
+    if(a==0) printf("on y-axis\n")
+    else normaltest(a, b);
+    if(b==0) printf("on x-axis\n")
+    else normaltest(a, b);
 
     return 0;
   }
 
+normaltest(x, y)
+  {
+  }
