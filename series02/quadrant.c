240b60875401b0368353d7098c5f1f7db64f6614
sebastian <sebastian.matkovich@gmail.com>
2019-03-12 18:43:34 +0100

    2.4

---
+++ series02/quadrant.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+double a,b;
+int main()
+  {
+    printf("enter a\n");
+    if(scanf("%lf", &a)!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    printf("enter b\n");
+    if(scanf("%lf", &b)!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+
+    if
+
+    return 0;
+  }
+
