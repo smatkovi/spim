ca770f1e4be1367854c227f6b37a890b99e444cc
sebastian <sebastian.matkovich@gmail.com>
2019-03-12 13:50:32 +0100

    added extremum

--- series02/curveSketching.c
+++ series02/curveSketching.c
@@ -17,6 +17,10 @@ int main()
     if(a==0 && b==0) printf("function is linear\n");
     else if(b==0) printf("function is parabola\n");
     else printf("function is a general squared function\n");
+
+    printf("extremum at (%lf, %lf)\n", -b/(2.*a), c + -b*b/(4.*a));
+    if(2.*a>0) printf("extremum is minimum\n");
+    else if(2.*a<0) printf("extremum is maximum\n");
     return 0;
   }
 
