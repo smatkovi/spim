85b60486d6d6749ad46460bc72353e84d0c927f2
sebastian <sebastian.matkovich@gmail.com>
2019-03-17 18:10:27 +0100

    3.2

---
+++ series03/lines.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+double s[2];
+int lines(double u[3], double v[3], double s[2]
+  {
+    
+  }
+int main()
+  {
+    double u[3],v[3];
+    printf("enter a\n");
+    if(scanf("%lf", &u[0])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    printf("enter b\n");
+    if(scanf("%lf", &u[1])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    printf("enter c\n");
+    if(scanf("%lf", &u[2])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+
+    printf("enter d\n");
+    if(scanf("%lf", &v[0])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    printf("enter e\n");
+    if(scanf("%lf", &v[1])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    printf("enter f\n");
+    if(scanf("%lf", &v[2])!=1)
+      {
+        printf("you didn't enter a number\n");
+        return 1;
+      }
+    
+    return 0;
+  }
+
