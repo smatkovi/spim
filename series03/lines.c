ca67c6f7771148fa1a834569beae1e0a3da87d17
sebastian <sebastian.matkovich@gmail.com>
2019-03-18 10:07:44 +0100

    3.2

--- series03/lines.c
+++ series03/lines.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 double s[2];
-int lines(double u[3], double v[3], double s[2]
+int lines(double u[3], double v[3], double s[2])
   {
-    
+   if(v[1]*u[0] - u[1]*v[0] == 0)
+     {
+       s[1]=(v[2]-v[0]*u[1]/u[0])/(v[1]-v[0]*u[1]/u[0]);
+       s[0]=u[2]/u[0] - u[1]/u[0]*s[1];
+       return -1;
+     }
+   else return 0;
   }
 int main()
   {
