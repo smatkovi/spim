6749dc4203ed43c04a8efba7672eeb2911c0b5f5
sebastian <sebastian.matkovich@gmail.com>
2019-03-24 15:16:46 +0100

    4.3

--- series04/frobeniusnorm.c
+++ series04/frobeniusnorm.c
@@ -1,24 +1,39 @@
 #include<stdio.h>
 #include<math.h>
-int i;
+int i, j;
 
 int main()
   {
     int dimm, dimn;
     
     double temp;
-    printf("enter n greater or equal to 0\n");
+    printf("enter m greater or equal to 0\n");
     scanf("%lf", &temp);
     dimm = (int) temp;
-    double n[dimm, dimn];
     if(dimm==temp && dimm>=0)
       {
-        for(i=0; i<dimmm; i++)
+        printf("enter n greater or equal to 0\n");
+        scanf("%lf", &temp);
+        dimn = (int) temp;
+
+        if(dimm==temp && dimm>=0)
           {
-            double temp;
-            printf("enter %i. number of %i\n", i+1, dimm);
-            scanf("%lf", &temp);
-            n[i,j] = temp;
+            double n[dimm][dimn];
+            for(i=0; i<dimm; i++)
+              {
+                for(j=0; j<dimn; j++)
+                  {
+                    double temp;
+                    printf("enter row %i, column %i of %i rows and %i columns\n", i+1, j+1, dimm, dimn);
+                    scanf("%lf", &temp);
+                    n[i][j] = temp;
+                  }
+	     }
+          }
+        else
+          {
+            printf("you didn't enter an integer greater or equal to 0\n");
+            return 1;
           }
       }
     else
