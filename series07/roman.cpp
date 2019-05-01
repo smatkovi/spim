a30152024ea023c470a4254bf0c54c7f5ae9ac7e
sebastian <sebastian.matkovich@gmail.com>
2019-05-01 23:16:29 +0200

    7.7

--- series07/roman.cpp
+++ series07/roman.cpp
@@ -1,11 +1,21 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <string.h>
 using std::cout;
 using std::endl;
 using std::min;
 using std::max;
+using std::abs;
 
+char* int2roman(int n);
+int roman2int(char* s);
+
+int main()
+{
+	return 0;
+}
+	
 char* int2roman(int n)
 {
 	char* r = new char[15];
@@ -79,8 +89,48 @@ char* int2roman(int n)
 	return r;
 }
 
-int main()
+int roman2int(char* s)
 {
-	return 0;
+	int n=0;
+	for(int i=0; abs(i)<abs(strlen(s)); i++)
+	{
+		while(s[i]=='M') n+=1000;	
+		if(s[i]=='C' && s[i+1]=='M')
+		{
+			n+=900;	
+			i++;
+		}
+		if(s[i]=='D') n+=500;	
+		if(s[i]=='C' && s[i+1]=='D')
+		{
+			n+=400;	
+			i++;
+		}
+		while(s[i]=='C') n+=100;	
+		if(s[i]=='X' && s[i+1]=='C')
+		{
+			n+=90;	
+			i++;
+		}
+		if(s[i]=='L') n+=50;	
+		if(s[i]=='X' && s[i+1]=='L')
+		{
+			n+=40;	
+			i++;
+		}
+		while(s[i]=='X') n+=10;	
+		if(s[i]=='I' && s[i+1]=='X')
+		{
+			n+=9;	
+			i++;
+		}
+		if(s[i]=='V') n+=5;	
+		if(s[i]=='I' && s[i+1]=='V')
+		{
+			n+=4;	
+			i++;
+		}
+		while(s[i]=='I') n+=1;	
+	}		
+	return n;
 }
-	
